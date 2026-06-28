9937d49c570af1b0c0acdcc8f123524e21b71a8d
Priyansh Thakar <priyanshthakar@gmail.com>
2026-06-28 17:57:37 -0400

    Add files via upload

---
+++ js/oop.js
@@ -0,0 +1,34 @@
+const output = document.getElementById("output");
+
+/* STEP 1a: Create a new object using a regular function */
+
+/* STEP 1b: Use the console to create a new vehicle object, and then invoke the function represented using .description() */
+
+/* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - 
+rewrite the above function, without returning anything. Capitalize the name of the function. */
+
+/* STEP 2b: Use the console to create a couple of different people, using the 'new' keyword, 
+and again invoking the .greeting() method for each person */
+
+/* STEP 3a: Build the complete constructor for the object Person (comment out the above function first).
+ Include name (first and last), age, gender, interests, bio (function), and greeting (function). */
+
+/* STEP 3b: Instantiate a new Person based on the above constructor */
+
+/* STEP 3c: Attempt to access the various properties of person1 using the console. */
+// person2['age']
+// person2.interests[1]
+// person2.bio()
+
+/* STEP 4a: Alternatively, you can use the Object() constructor to create an object. eg. car*/
+
+/* STEP 4b: Once 'car' is created, add various properties and methods… */
+
+/* STEP 4c: Change some of the properties of 'car' in the console, then invoke the car.fun() function */
+
+/* STEP 5a: Yet another approach is to use the create() method. 
+Let's see how the above car object can be used to create another object */
+
+/* STEP 5b: Output to the paragraph anotherCar.brand - you will see that it has retained the properties of the original object. */
+
+// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
